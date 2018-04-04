cfea9a124030ddb1aa1cb23668a5b6950106b00f
gengzs <gengzhongshu@163.com>
2018-04-04 18:24:41 +0800

    no message

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+Route::get('now', function(){
+    return date("Y-m-d H:i:s");
+});
+Route::post('/home', 'HomeController@indexPost');
