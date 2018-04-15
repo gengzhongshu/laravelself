2308036d598d2d74b6c59e78b0b99a6266f8c28b
DESKTOP-RCMV68C\耿中书 <gengzhongshu@163.com>
2018-04-16 00:00:54 +0800

    列表页面

--- resources/views/admin/customer/index.blade.php
+++ resources/views/admin/customer/index.blade.php
@@ -4,5 +4,7 @@
     <div class="page-header headline">
         <h1>添加顾客</h1>
     </div><!--.headline end-->
-
+    <div style="width: 500px;height: 500px;">
+        {!! $customer->appends(['search_sid'=>$customer->name, 'search_title'=>$customer->phone])->render() !!}
+    </div>
 @endsection
