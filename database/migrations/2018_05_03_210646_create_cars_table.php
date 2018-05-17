9a621565003164b523c68dc1b633c8931acc10e8
DESKTOP-RCMV68C\耿中书 <gengzhongshu@163.com>
2018-05-17 21:34:45 +0800

    no message

---
+++ database/migrations/2018_05_03_210646_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('car_name');
+            $table->string('paizhao');
+            $table->string('pailiang');
+            $table->integer('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cars');
+    }
+}
